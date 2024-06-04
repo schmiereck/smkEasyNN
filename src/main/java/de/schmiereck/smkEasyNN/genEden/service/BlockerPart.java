dcf105645d74c15a41b2415f917e72ddab446e93
Thomas Schmiereck <thomas@schmiereck.de>
2024-06-04 08:34:28 +0200

    GenNet: Geneden Implementation: Genetic-Parts persistent.

---
+++ src/main/java/de/schmiereck/smkEasyNN/genEden/service/BlockerPart.java
@@ -0,0 +1,7 @@
+package de.schmiereck.smkEasyNN.genEden.service;
+
+public class BlockerPart extends Part {
+    public BlockerPart(double[] visibleValueArr) {
+        super(visibleValueArr);
+    }
+}
